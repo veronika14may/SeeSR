d796a912d3c2269ed58cc6ad182328db83e337aa
Maiskaya Veronika <144445023+veronika14may@users.noreply.github.com>
2026-05-17 12:38:35 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -70,8 +70,8 @@ class Predictor(BasePredictor):
         controlnet.requires_grad_(False)
 
         if is_xformers_available():
-            unet.enable_xformers_memory_efficient_attention()
-            controlnet.enable_xformers_memory_efficient_attention()
+            # unet.enable_xformers_memory_efficient_attention()
+            # controlnet.enable_xformers_memory_efficient_attention()
         else:
             raise ValueError("xformers is not available. Make sure it is installed correctly")
 
